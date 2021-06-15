21342c595272e7fd19bc76384a7fc25e4d2488cf
mauriciomateus <helvert.mateus@gmail.com>
2021-06-14 21:43:52 -0500

    Page title for browser's title bar being passed from router

--- router/router.js
+++ router/router.js
@@ -10,26 +10,48 @@ const routes = [
   {
     path: '/rest-auth/register',
     name: 'Register',
-    component: Register
+    component: Register,
+    meta: {
+      title: 'Create Account'
+    }
   },
   {
     path: '/rest-auth/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    meta: {
+      title: 'Login'
+    }
   },
   {
     path: '/rest-auth/password-reset',
     name: 'PasswordReset',
-    component: PasswordReset
+    component: PasswordReset,
+    meta: {
+      title: 'Reset Your Password'
+    }
   },
   {
     path: '*',
     name: 'NotFound',
-    component: NotFound
+    component: NotFound,
+    meta: {
+      title: 'Page Not Found'
+    }
   }
 ]
 const router = new VueRouter({
   mode: 'history',
   routes
+})
+
+// Update page title: https://stackoverflow.com/questions/51639850/how-to-change-page-titles-when-using-vue-router
+const DEFAULT_TITLE = 'Crowdbotics'
+router.afterEach((to, from) => {
+  // Use next tick to handle router history correctly
+  // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+  Vue.nextTick(() => {
+    document.title = to.meta.title || DEFAULT_TITLE
+  })
 })
 export default router
