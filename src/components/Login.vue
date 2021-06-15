f2a9c129bc6ee1dbf1396f94b7d5f8a258135578
mauriciomateus <helvert.mateus@gmail.com>
2021-06-14 21:46:40 -0500

    Updated login and register forms to use the same form with different props to define the api end point and other info

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,38 @@
 <template>
   <AuthLayout>
-    Login
+    <RegisterAndLoginContainer
+      title="Login to Dashboard"
+    >
+      <AuthForm
+        end-point="/rest-auth/login/"
+        button-text="Login"
+      />
+      <template v-slot:footer>
+        <p>
+          Don't have an account?
+          <router-link
+            :to="{name:'Register'}"
+            class="text-brand-color font-semibold hover:underline"
+          >
+            Register Now
+          </router-link>
+        </p>
+      </template>
+    </RegisterAndLoginContainer>
   </AuthLayout>
 </template>
 
 <script>
 // @vue/component
+import AuthForm from './AuthForm'
 import AuthLayout from './AuthLayout'
+import RegisterAndLoginContainer from './RegisterAndLoginContainer'
 
 export default {
   components: {
-    AuthLayout
+    AuthForm,
+    AuthLayout,
+    RegisterAndLoginContainer
   }
 }
 </script>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,96 +1,41 @@
 <template>
   <AuthLayout>
-    <div class="col-span-7 bg-white py-12 px-8 text-sm flex flex-col items-center">
-      <h2 class="text-gray-700 text-2xl">
-        Create account
-      </h2>
-      <form
-        class="w-full"
-        @submit.prevent="handleFormSubmit"
-      >
-        <div class="w-full mt-3">
-          <label
-            for="email"
-            class="block w-full text-gray-700"
-          >Email*</label>
-          <!--Type set as text just to see the api validation -->
-          <input
-            id="email"
-            v-model="form.email"
-            type="text"
-            name="email"
-            class="w-full rounded-lg border-gray-500"
+    <RegisterAndLoginContainer
+      title="Create account"
+    >
+      <AuthForm
+        end-point="/rest-auth/registration/"
+        button-text="Sign Up"
+      />
+      <template v-slot:footer>
+        <p>
+          Already have an account?
+          <router-link
+            :to="{name:'Login'}"
+            class="text-brand-color font-semibold hover:underline"
           >
-          <RegistrationError :error="errors['email']" />
-        </div>
-        <div class="w-full mt-3">
-          <label
-            for="password"
-            class="block w-full text-gray-700"
-          >Password*</label>
-          <input
-            id="password"
-            v-model="form.password"
-            type="password"
-            name="password"
-            class="w-full rounded-lg border-gray-500"
-          >
-          <RegistrationError :error="errors['password']" />
-        </div>
-        <!--        <div class="w-full mt-3">-->
-        <!--          <label-->
-        <!--            for="password_confirm"-->
-        <!--            class="block w-full text-gray-700"-->
-        <!--          >Repeat Password*</label>-->
-        <!--          <input-->
-        <!--            id="password_confirm"-->
-        <!--            type="password"-->
-        <!--            name="password_confirm"-->
-        <!--            class="w-full rounded-lg border-gray-500"-->
-        <!--          >-->
-        <!--          <RegistrationError :error="errors['password_confirm']" />-->
-        <!--        </div>-->
-        <div class="flex justify-center w-full">
-          <button
-            type="submit"
-            class="uppercase py-3 px-20 bg-brand-bg text-white rounded-3xl font-bold mt-4 focus:outline-none focus:ring-4 focus:ring-green-200"
-          >
-            Sign Up
-          </button>
-        </div>
-      </form>
-    </div>
+            Login now
+          </router-link>
+        </p>
+      </template>
+    </RegisterAndLoginContainer>
   </AuthLayout>
 </template>
 
 <script>
 // @vue/component
 import AuthLayout from './AuthLayout'
-import RegistrationError from './RegistrationError'
+import AuthError from './AuthError'
 import axios from 'axios'
+import RegisterAndLoginContainer from './RegisterAndLoginContainer'
+import AuthForm from './AuthForm'
 
 export default {
   name: 'RegistrationForm',
   components: {
     AuthLayout,
-    RegistrationError
-  },
-  data () {
-    return {
-      form: {},
-      errors: {}
-    }
-  },
-  methods: {
-    handleFormSubmit () {
-      axios.post('/rest-auth/registration/', this.form)
-        .then(response => {
-          console.log(response)
-        })
-        .catch(error => {
-          this.errors = error.response.data
-        })
-    }
+    RegisterAndLoginContainer,
+    AuthForm
   }
 }
 </script>
