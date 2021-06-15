17e085bb4de826f6fbf535dcf03fa4a798f0b05f
mauriciomateus <helvert.mateus@gmail.com>
2021-06-15 05:27:06 -0500

    Working on dashboard authentication

---
+++ src/components/DashboardLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col">
+    <div class="h-8 bg-gray-900 h-12 px-5 flex justify-between items-center">
+      <router-link
+        :to="{name:'Dashboard.AppsIndex'}"
+        class="flex overflow-hidden h-5"
+      >
+        <img
+          src="https://cb-dash.s3.amazonaws.com/img/cb-footer.png"
+          alt="Dashboard Home"
+          class="block h-full w-auto"
+        >
+      </router-link>
+    </div>
+    <div class="p-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+// @vue/component
+export default {
+
+}
+</script>
