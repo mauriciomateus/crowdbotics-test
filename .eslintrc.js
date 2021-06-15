9c9986e5213bbd68114f5eafcf0362c185cdc881
mauriciomateus <helvert.mateus@gmail.com>
2021-06-14 19:43:44 -0500

    Configured eslint plugin Vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/standard'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars' : 'warn',
+    'no-unreachable' : 'warn'
   }
 }
