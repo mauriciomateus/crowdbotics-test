d951859191107350b315107d87ca475d434c083d
mauriciomateus <helvert.mateus@gmail.com>
2021-06-14 21:45:27 -0500

    Created custom layout component to hold the login and register forms

---
+++ src/components/RegisterAndLoginContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-span-7 bg-white py-12 px-8 text-sm flex flex-col items-center">
+    <h2 class="text-gray-700 text-2xl">
+      {{ title }}
+    </h2>
+    <slot />
+    <footer class="mt-auto">
+      <slot name="footer" />
+    </footer>
+  </div>
+</template>
+
+<script>
+// @vue/component
+export default {
+  props: {
+    title: {
+      type: String,
+      default: String
+    }
+  }
+}
+</script>
