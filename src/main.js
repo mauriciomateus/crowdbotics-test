01c1f8eff109fc94c610ced81940bf9cf2f0ad8e
mauriciomateus <helvert.mateus@gmail.com>
2021-06-15 19:38:44 -0500

    Added access token from local storage as default axios header

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import store from './store/store'
 import router from './../router/router'
 
 Vue.config.productionTip = false
+const accessToken = localStorage.getItem('accessToken')
 axios.defaults.baseURL = 'https://hiring-example-25770.botics.co'
+axios.defaults.headers.common.Authorization = `token ${accessToken}`
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
